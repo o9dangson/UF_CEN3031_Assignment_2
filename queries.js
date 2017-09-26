393e1eec5d62127b6a4bbb1b6d5975a13953bd42
o9dangson <o9dangson@ufl.edu>
2017-09-25 21:24:22 -0400

    Corrected functions

--- queries.js
+++ queries.js
@@ -1,10 +1,11 @@
 /* Fill out these functions using Mongoose queries*/
-var mongoose = require('mongoose'), 
-    Schema = mongoose.Schema,
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Listing = require('./ListingSchema.js'); 
+var config = require('./config');
+//var allListings = require('./listings.json');
 
-
+mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
   /* 
@@ -16,40 +17,43 @@ var findLibraryWest = function() {
     console.log(listing);
    });
 };
+
+
 var removeCable = function() {
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
-   Listing.find({code: 'CABL'}, function(err, listing){
-    if(err) throw err;
-    listing.remove(function(err){
-      if(err) throw err;
-      console.log('Listing successfully deleted!');
-    });
-   });
+   Listing.findOneAndRemove({ code: 'CABL' }, function(err) {
+  if (err) throw err;
+  // we have deleted the user
+  console.log('\nListing deleted!');
+  });
 };
+
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
-   Listing.find({name: 'Phelps Laboratory'}, function(err, listing){
-    if(err) throw err;
-    //Code here to change the address
-    listing.address = '1953 MUSEUM RD GAINESVILLE, FL 32611, United States';
-    //save
-    listing.save(function(err){
-      if(err) throw err;
-      console.log('Listing successfully updated');
-    });
-   });
+  Listing.findOneAndUpdate({name: 'Phelps Laboratory'}, { address: '1953 MUSEUM RD GAINESVILLE, FL 32611, United States' }, function(err, listing) {
+  if (err) throw err;
+
+  // we have the updated user returned to us
+  console.log(listing);
+  });
 };
+
+
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({}, function(err, listings){
+  if(err) throw err;
+  console.log(listings);
+});
 };
 
 findLibraryWest();
